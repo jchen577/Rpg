409222f97cd96a727c3234dbad9c4b78f009ef95
jchen577 <95502719+jchen577@users.noreply.github.com>
2024-09-17 14:16:22 -0700

    updated credits

--- src/scenes/Start.js
+++ src/scenes/Start.js
@@ -5,15 +5,15 @@ class Start extends Phaser.Scene{
 
     create(){
         this.fontSize = 48
-        this.offset = 10
+        this.offset = 0.05
         this.bg = this.add.sprite(config.width/2,config.height/2,"loadbg");
         this.playerL = this.add.sprite(config.width/2-300,config.height/2, "playerS",6).setScale(8);
-        this.playText2 = this.add.bitmapText(config.width/2-this.fontSize-this.offset/2,config.height/2-this.fontSize+this.offset/2,'gem_font','PLAY',this.fontSize);
-        this.playText = this.add.bitmapText(config.width/2-this.fontSize,config.height/2-this.fontSize,'gem_font','PLAY',this.fontSize);
+        this.playText2 = this.add.bitmapText(config.width/2,config.height/2,'gem_font','PLAY',this.fontSize).setOrigin(1+this.offset,1+this.offset);
+        this.playText = this.add.bitmapText(config.width/2,config.height/2,'gem_font','PLAY',this.fontSize).setOrigin(1,1);
         this.playText2.setCharacterTint(0,-1,true,0x000000);
-        this.cText2 = this.add.bitmapText(config.width/2-(this.playText.width/4)-this.fontSize-this.offset/2,config.height/2+100-this.fontSize+this.offset/2,'gem_font','CREDITS',this.fontSize);
+        this.cText2 = this.add.bitmapText(config.width/2,config.height/2,'gem_font','CREDITS',this.fontSize).setOrigin(0.5+this.offset/1.5+this.playText.width/100/4,-1+this.offset);
         this.cText2.setCharacterTint(0,-1,true,0x000000);
-        this.cText = this.add.bitmapText(config.width/2-(this.playText.width/4)-this.fontSize,config.height/2+100-this.fontSize,'gem_font','CREDITS',this.fontSize);
+        this.cText = this.add.bitmapText(config.width/2,config.height/2,'gem_font','CREDITS',this.fontSize).setOrigin(0.5+this.playText.width/100/4,-1);
         this.playText.setInteractive({
             useHandCursor: true,
         });
--- src/scenes/credits.js
+++ src/scenes/credits.js
@@ -4,5 +4,10 @@ class Credit extends Phaser.Scene{
     }
 
     create(){
+        this.fontSize = 48
+        this.playText = this.add.bitmapText(config.width/2,config.height/2,'gem_font','Developers: GDLearns',this.fontSize).setOrigin(0.6,2);
+        this.credText = this.add.bitmapText(config.width/2,config.height/2,'gem_font','Art By:',this.fontSize).setOrigin(2.5,0);
+        this.credText = this.add.bitmapText(config.width/2,config.height/2,'gem_font','Kasayas, Game Endeavor,',this.fontSize).setOrigin(0.4,0);
+        this.credText = this.add.bitmapText(config.width/2,config.height/2,'gem_font','Pixel-Boy and AAA',this.fontSize).setOrigin(0.6,-1);
     }
 }
